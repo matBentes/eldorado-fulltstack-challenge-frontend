6fca4f08b366a56f5e24c825f457f6fb8c8f5d6b
Mateus Bentes <mateus1337bentes@gmail.com>
2022-10-23 15:50:37 -0400

    feat: created new route for add book

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddBookComponent } from './add-book/add-book.component';
 import { BookComponent } from './book/book.component';
 
 const routes: Routes = [
-  { path: 'book', component: BookComponent }
+  { path: 'book', component: BookComponent },
+  { path: 'book/new', component: AddBookComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
