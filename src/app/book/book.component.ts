858ce8af90e75e7a9b4c42dbb13aa8f290dad2da
Mateus Bentes <mateus1337bentes@gmail.com>
2022-10-22 22:31:06 -0400

    feat: create a book calling child function

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AddBookComponent } from '../add-book/add-book.component';
 import { Book } from '../book';
 import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  styleUrls: ['./book.component.scss'],
 })
 export class BookComponent implements OnInit {
   books: Book[] = []
 
+  @ViewChild(AddBookComponent, { static: true })
+  child!: AddBookComponent;
+  
   constructor(private bookService: BookService) { }
 
   ngOnInit(): void {
@@ -20,4 +24,7 @@ export class BookComponent implements OnInit {
     this.bookService.getBook().subscribe((books) => this.books = books)
   }
 
+  addBookFromChild() {
+    this.child.addBook(this.child.book)
+  }
 }
