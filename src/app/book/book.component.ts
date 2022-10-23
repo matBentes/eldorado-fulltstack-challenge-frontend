a4388fa585b971593e28351437e1c1bc4abeda30
Mateus Bentes <mateus1337bentes@gmail.com>
2022-10-23 15:48:52 -0400

    refactor: removed child logic from parent

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { AddBookComponent } from '../add-book/add-book.component';
 import { Book } from '../book';
 import { BookService } from '../book.service';
-import { EditBookComponent } from '../edit-book/edit-book.component';
 
 @Component({
   selector: 'app-book',
@@ -10,40 +8,22 @@ import { EditBookComponent } from '../edit-book/edit-book.component';
   styleUrls: ['./book.component.scss'],
 })
 export class BookComponent implements OnInit {
-  books: Book[] = []
+  books: Book[] = [];
 
-  @ViewChild(AddBookComponent)
-  child!: AddBookComponent;
-
-  @ViewChild(EditBookComponent)
-  childEdit!: EditBookComponent;
-
-  constructor(private bookService: BookService) { }
+  constructor(private bookService: BookService) {}
 
   ngOnInit(): void {
-    this.getBooks()
+    this.getBooks();
   }
 
   getBooks() {
-    this.bookService.getBook().subscribe((books) => this.books = books)
-  }
-
-  addBookFromChild() {
-    this.child.addBook(this.child.book).subscribe((data) => this.books.push(data))
-  }
-
-  deleteBookFromChild(isbn: string) {
-    this.child.deleteBook(isbn)
-  }
-
-  updateBookFromChild(book: Book) {  
-    this.childEdit.updateBook(book)
+    this.bookService.getBook().subscribe((books) => (this.books = books));
   }
 
   confirmDelete(isbn: string, index: number) {
-    if (confirm("Are you sure to delete?")) { 
-      this.deleteBookFromChild(isbn)
-      this.books.splice(index, 1)
+    if (confirm('Are you sure to delete?')) {
+      this.bookService.deleteBook(isbn).subscribe();
+      this.books.splice(index, 1);
     }
   }
 }
